7f833b0c1a08b2744e6e492db2e8fab2da3f9acc
agosmay <agostinamgatti@gmail.com>
2022-10-27 14:42:32 -0300

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DataFetching from './componentes/DataFetching'
+
+function App() {
+  return (
+    <div className="App">
+		<h1>Hola mundo</h1>
+		<DataFetching />
+		
+    </div>
+  );
+}
+
+export default App;
